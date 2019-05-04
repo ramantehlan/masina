f5a3ecb131e6de6c5422b680e15e8dad6af137a1
Piyush <piyush@viewlift.com>
2019-05-04 21:53:47 +0530

    cloud-backend-service

--- backend-services/cloud-service/handler.js
+++ backend-services/cloud-service/handler.js
@@ -1,14 +1,66 @@
 'use strict';
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+var AWS = require("aws-sdk");
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.hello = async (event) => {
+
+  AWS.config.update({
+    region: "us-east-1"
+  });
+
+  var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+  var myTable = 'insuranceTable';
+  try{
+
+    var xy=event["queryStringParameters"]["xy"];
+    var yz=event["queryStringParameters"]["yz"];
+    var zx=event["queryStringParameters"]["zx"];
+    var impactVal=(parseFloat(xy)+parseFloat(yz)+parseFloat(zx)).toString();
+
+    var params = {
+      TableName: myTable,
+      Item: {'carNumber' : {S: '100329'}, 'xy' : {S: xy}, 'yz' : {S: yz}, 'zx' : {S: zx}, 'impactValue' : {S: impactVal}}
+    };
+  
+    var paramsValue = await post(ddb,params);
+    console.log(paramsValue);
+  
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Impact Values sent to Insurance Company Cloud',
+        xy,yz,zx,impactValue:parseFloat(parseFloat(xy)+parseFloat(yz)+parseFloat(zx))
+      }),
+    };
+  }catch(e){
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        "errorMessage":e
+      }),
+    };
+  } 
 };
+
+var post = function (ddb,params) {
+  return new Promise(function(resolve,reject){
+
+    ddb.putItem(params, function(err, data) {
+      if (err) {
+        console.log("Error", err);
+        reject(err);
+      } else {
+        console.log("Success", data);
+        resolve("success")
+      }
+    });
+
+  })
+ 
+}
+
+
+
+
+
